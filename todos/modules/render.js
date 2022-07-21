8e2296bdcf8d8adfb5e79f93f3b0e1752068d435
Quynh Le <107383112+QuynhLeSke@users.noreply.github.com>
2022-07-21 23:49:03 +0700

    Add task to project

--- todos/modules/render.js
+++ todos/modules/render.js
@@ -8,9 +8,24 @@ export const displayProjBar = () => {
     if (e.key == "Enter" && e.target.value) {
       e.preventDefault();
       const newProj = document.createElement("button");
+      const delicon = document.createElement("a");
+      const delimg = document.createElement("img");
+
+      delicon.setAttribute("href", "#");
+      delimg.setAttribute("src", "../vendor/del-icon.png");
+      delimg.classList.add("del-icon");
+
       newProj.textContent = e.target.value;
+      newProj.appendChild(delicon);
+      delicon.appendChild(delimg);
       projCtn.appendChild(newProj);
+
       e.target.value = "";
     }
   });
+};
+export const getProjectTitleFromDOM = () => {
+  const projName = document.getElementById("proj-name");
+  const titleValue = projName.value;
+  return titleValue;
 };
--- todos/todo.js
+++ todos/todo.js
@@ -1,21 +1,30 @@
 "use strict";
-import { displayProjBar } from "../todos/modules/render.js";
+// import { displayProjBar } from "../todos/modules/render.js";
 
-const projAdd = document.getElementById("proj-add");
+// const projAdd = document.getElementById("proj-add");
 
-const addTask = document.getElementById("addTask");
-const formTask = document.getElementById("formTask");
-const addNewTask = document.getElementById("addNewTask");
+// const addTask = document.getElementById("addTask");
+// const formTask = document.getElementById("formTask");
+// const addNewTask = document.getElementById("addNewTask");
+// const cancle = document.getElementById("cancle");
 
-displayProjBar();
+// displayProjBar();
 
-addTask.addEventListener("click", () => {
-  formTask.classList.remove("hidden");
-  formTask.classList.add("show");
-});
+// addTask.addEventListener("click", () => {
+//   formTask.classList.remove("hidden");
+//   formTask.classList.add("show");
+// });
 
-addNewTask.addEventListener("click", () => {
-  const title = document.getElementById("title");
-  const des = document.getElementById("des");
-  const dateTask = document.getElementById("dateTask");
-});
+// addNewTask.addEventListener("click", () => {
+//   const title = document.getElementById("title");
+//   const des = document.getElementById("des");
+//   const dateTask = document.getElementById("dateTask");
+// });
+
+// cancle.addEventListener("click", () => {
+//   formTask.classList.remove("show");
+//   formTask.classList.add("hidden");
+// });
+import UI from "../todos/modules/UI.js";
+
+UI.loadApp();
