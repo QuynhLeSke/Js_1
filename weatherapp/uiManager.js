6af92cb93bdaec15ccc0b9d72691e5b3c09ff6ee
Quynh Le <107383112+QuynhLeSke@users.noreply.github.com>
2022-07-22 04:29:13 +0700

    Add weatherapp

---
+++ weatherapp/uiManager.js
@@ -0,0 +1,39 @@
+const uiManager = (function uiManager() {
+  // buffer DOM
+  const weatherCard = document.querySelector("div.weather-card");
+
+  const errorCard = document.querySelector("div.error-card");
+  const loader = document.querySelector("div.loader");
+  const location = document.querySelector(".location");
+  const currTemperature = document.querySelector(".curr-temperature");
+  const icon = document.querySelector("img.weather-icon");
+  const feelsLike = document.querySelector(".feels-like");
+  const weatherDescription = document.querySelector(".weather-conditions");
+
+  function updateCard(weatherObj) {
+    errorCard.classList.add("hide");
+    weatherCard.classList.remove("hide");
+    location.textContent = weatherObj.location;
+    currTemperature.textContent = `${weatherObj.temp}°`;
+    feelsLike.textContent = `Feels like ${weatherObj.feelsLike}°`;
+    weatherDescription.textContent = weatherObj.weatherDescription;
+  }
+
+  function notFoundError() {
+    weatherCard.classList.add("hide");
+    errorCard.classList.remove("hide");
+  }
+
+  function startLoading() {
+    weatherCard.classList.add("hide");
+    errorCard.classList.add("hide");
+    loader.classList.remove("hide");
+  }
+
+  function endLoading() {
+    loader.classList.add("hide");
+  }
+  return { updateCard, notFoundError, startLoading, endLoading };
+})();
+
+export { uiManager };
---
+++ weatherapp/weatherapp.js
@@ -0,0 +1,96 @@
+import { uiManager } from "./uiManager.js";
+
+const searchBar = document.querySelector("input");
+const form = document.querySelector("form");
+const toggle = document.querySelector("input.toggle");
+let metricWeather;
+let imperialWeather;
+let currentWeather;
+
+class WeatherObject {
+  constructor(
+    location,
+    temp,
+    feelsLike,
+    humidity,
+    weatherDescription,
+    weatherMain,
+    icon
+  ) {
+    this.location = location;
+    this.temp = temp.toFixed();
+    this.feelsLike = feelsLike.toFixed();
+    this.humidity = humidity;
+    this.weatherMain = weatherMain;
+    this.weatherDescription = weatherDescription;
+    this.icon = icon;
+  }
+}
+
+async function makeApiCall(cityName) {
+  const apiKey = "7fdbc7aa6a75a22aa698b65b12ba530c";
+
+  const metricResponse = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`,
+    { mode: "cors" }
+  );
+  const metricData = await metricResponse.json();
+
+  const imperialResponse = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`,
+    { mode: "cors" }
+  );
+  const imperialData = await imperialResponse.json();
+
+  if (metricData.cod === "404") {
+    return false;
+  }
+  console.log(metricData);
+
+  metricWeather = new WeatherObject(
+    `${metricData.name}, ${metricData.sys.country}`,
+    metricData.main.temp,
+    metricData.main.feels_like,
+    metricData.main.humidity,
+    metricData.weather[0].description,
+    metricData.weather[0].main,
+    metricData.weather[0].icon
+  );
+
+  imperialWeather = new WeatherObject(
+    `${imperialData.name}, ${imperialData.sys.country}`,
+    imperialData.main.temp,
+    imperialData.main.feels_like,
+    imperialData.main.humidity,
+    imperialData.weather[0].description,
+    imperialData.weather[0].main,
+    imperialData.weather[0].icon
+  );
+  return true;
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  uiManager.startLoading();
+
+  makeApiCall(searchBar.value, "C").then((result) => {
+    searchBar.value = "";
+    uiManager.endLoading();
+    if (result === false) {
+      console.log("not found");
+      uiManager.notFoundError();
+      return;
+    }
+    currentWeather = metricWeather;
+    uiManager.updateCard(currentWeather);
+  });
+});
+
+toggle.addEventListener("click", () => {
+  if (currentWeather === metricWeather) {
+    currentWeather = imperialWeather;
+  } else {
+    currentWeather = metricWeather;
+  }
+  uiManager.updateCard(currentWeather);
+});
